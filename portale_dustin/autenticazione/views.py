b0bcef76ca5ff1518ed71fee22974953a9443eb2
Elvino00 <el.buonanno@libero.it>
2025-07-17 17:46:10 +0200

    Aggiunta email nella registrazione

--- portale_dustin/autenticazione/views.py
+++ portale_dustin/autenticazione/views.py
@@ -1,37 +1,41 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
 
 # Create your views here.
-def register_view(request):
+def register_view(request): #vista per la registrazione
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Registrazione completata con successo!. Ora puoi effettuare il log in.")
+        form = UserRegistrationForm(request.POST) #form personalizzato creato in forms.py
+        if form.is_valid(): #salvataggio nuovo utente se il form è valido. Se l'utente viene registrato con successo, si fa il redirect alla pagina di registrazione
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            messages.success(request, "Registrazione completata con successo! Ora puoi effettuare il log in.")
             return redirect('register') 
     else:
-        form = UserCreationForm()
-    return render(request, "autenticazione/registrazione.html" , {"form" : form})
+        form = UserRegistrationForm()
+    return render(request, "autenticazione/registrazione.html" , {"form" : form}) #passaggio del form al template html di registrazione
 
-def login_view(request):
+def login_view(request): #vista per il login
     if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
+        form = AuthenticationForm(data=request.POST) #form di autenticazione di Django
         if form.is_valid():
-            user = form.get_user()
+            user = form.get_user() #estrazione utente per passarlo alla home e mostrare messaggio di benvenuto
             login(request, user)
-            return redirect('home')
+            return redirect('home') #redirect alla home (home.html)
     else:
         form = AuthenticationForm()
-    return render(request, "autenticazione/login.html" , {"form" : form})
+    return render(request, "autenticazione/login.html" , {"form" : form}) 
 
-@login_required
+@login_required #decorator che porta alla home se il login è avvenuto con successo
 def home_view(request):
     return render(request, "autenticazione/home.html")
 
-def logout_view(request):
+def logout_view(request): #view di logout
+    request.session.flush()
     logout(request)
     messages.success(request, "Logout effettuato con successo!")
-    return redirect('login')
+    return redirect('login') #dopo aver effettuato il logout, ritorna alla pagina di login
